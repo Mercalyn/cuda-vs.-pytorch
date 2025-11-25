8b62881437bf81ac7ae60f106b593f82eec446b3
Em Thomas <alliancerune@gmail.com>
2025-11-24 21:03:43 -0700

    Add files via upload

--- pytorch.py
+++ pytorch.py
@@ -1,21 +1,29 @@
 import torch
 import os
-devc = torch.device(type="cuda")
-dtype = torch.float32
-torch.set_printoptions(sci_mode=False)
-os.system("cls")
 
-# setup
+# config
 NUM_X_BLOCKS = int(20E3)
 THREAD_1D_LEN = 32
 TOTAL_SIZE = (THREAD_1D_LEN**2)
 ITER = int(16E3)
+
+# setup
+devc = torch.device(type="cuda")
+dtype = torch.float32
+torch.set_printoptions(sci_mode=False)
+os.system("cls")
 start_event = torch.cuda.Event(enable_timing=True)
 end_event = torch.cuda.Event(enable_timing=True)
+gconf = {
+    "dtype": dtype,
+    "device": devc
+}
+size3d = [NUM_X_BLOCKS, THREAD_1D_LEN, THREAD_1D_LEN] # size [20000, 32, 32]
+size2d = [THREAD_1D_LEN, THREAD_1D_LEN] # size [32, 32]
 
+# def kernels
 def proc(A, B, C, G):
     G = (A @ B) + C
-    return G
 
 # prefill -- domain=[0, 1)
 print(f"prefilling...")
@@ -23,18 +31,18 @@ print(f"prefilling...")
 A needs to be a 3d tensor was due to the kernel caching results and not actually
 processing all the data, thus A will contain random data to force it to process thoroughly
 """
-A = torch.rand([NUM_X_BLOCKS, THREAD_1D_LEN, THREAD_1D_LEN], dtype=dtype, device=devc) # size [32, 32]
-B = torch.rand([THREAD_1D_LEN, THREAD_1D_LEN], dtype=dtype, device=devc) # size [32, 32]
-C = torch.rand([THREAD_1D_LEN, THREAD_1D_LEN], dtype=dtype, device=devc) # size [32, 32]
-G = torch.zeros([NUM_X_BLOCKS, THREAD_1D_LEN, THREAD_1D_LEN], dtype=dtype, device=devc) # size [20000, 32, 32]
+A = torch.rand(size3d, **gconf)
+B = torch.rand(size2d, **gconf)
+C = torch.rand(size2d, **gconf)
+G = torch.zeros(size3d, **gconf)
 
 # timer start
 torch.cuda.synchronize()
 start_event.record()
 
+# process
 print(f"processing...")
 for _ in range(ITER):
-    # process
     proc(A, B, C, G)
 
 # timer end
